9e2224a73854adbb545c716120594e868b3f8fcf
Jackson Naufal <Jackson.naufal@mths.ca>
2022-02-25 22:39:49 +0000

    AreaPerimeterCalculator

---
+++ areaPerimeterCalculator.cpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2022 Jackson Naufal All rights reserved
+// Created By Jackson Naufal
+// Created On Feb 2022
+// This is a "Hello, World!" program with proper style"
+
+#include <cmath>
+
+#include <iostream>
+
+int main() {
+    std::cout << "If your circle has the radius of:" << std::endl;
+    std::cout << "15mm" << std::endl;
+    std::cout <<  std::endl;
+    std::cout << "Area is " << (M_PI * pow(15, 2)) << "mm²."  << std::endl;
+    std::cout << "Perimeter is "<< (2 *M_PI * 15) << "cm."<< std::endl;
+    std::cout <<  std::endl;
+    std::cout << "Calculation is complete!" << std::endl;
+}
